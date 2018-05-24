b980d774cce3ad85da91c3d8583018a59600412b
berkin768 <berkinnakaydinn@gmail.com>
2018-05-24 19:18:46 +0300

    Working on EventDB

--- app/src/main/java/com/example/deniz/plaxo2/activities/AddEventActivity.java
+++ app/src/main/java/com/example/deniz/plaxo2/activities/AddEventActivity.java
@@ -7,14 +7,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.deniz.plaxo2.R;
+import com.example.deniz.plaxo2.model.Event;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Deniz on 19.05.2018.
@@ -30,6 +33,7 @@ public class AddEventActivity extends AppCompatActivity {
     int start_minute;
     int end_hour;
     int end_minute;
+    EditText title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +45,10 @@ public class AddEventActivity extends AppCompatActivity {
         startTime = (TimePicker) findViewById(R.id.start_time_picker);
         endTime = (TimePicker) findViewById(R.id.end_time_picker);
         addEvent_fab = (FloatingActionButton) findViewById(R.id.addEvent_fab);
+        title = (EditText) findViewById(R.id.event_title);
 
         long selectedTime = intent.getLongExtra("Date",0);
-        Date selectedDate = new Date(selectedTime);
+        final Date selectedDate = new Date(selectedTime);
         Log.d("selectedTime", selectedDate+"");
 
         dateTextview.setText(selectedDate+"");
@@ -78,18 +83,24 @@ public class AddEventActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if((start_hour == end_hour && end_minute > start_minute) || start_hour < end_hour){
                     Toast.makeText(getBaseContext(), "OK", Toast.LENGTH_SHORT).show();
-                    System.out.print(start_hour);
-                    System.out.print(start_minute);
-                    System.out.print(end_hour);
-                    System.out.print(end_minute);
+
+                    List<Event> allEvents = Event.listAll(Event.class);
+                    int size = allEvents.size();
+
+                    Event event = new Event();
+                    event.setEventId(size+1);
+                    event.setDate(selectedDate);
+                    event.setEndHour(end_hour);
+                    event.setEndMinute(end_minute);
+                    event.setStartHour(start_hour);
+                    event.setStartMinute(start_minute);
+                    event.setTitle(title.getText()+"");
+                    event.save();
+                    finish();
                 }
                 else{
                     Toast.makeText(getBaseContext(), "Cannot choose before", Toast.LENGTH_SHORT).show();
-
-
                 }
-
-
             }
         });
     }
